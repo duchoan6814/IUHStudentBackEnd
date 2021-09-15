54840138914381fcad793c1076f653ea44107924
Hoan Truong <hoantruong6814@gmail.com>
2021-09-15 19:16:26 +0700

    first commit, create project

---
+++ src/main/java/com/iuh/IUHStudent/IuhStudentApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.IUHStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuhStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuhStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iuh/IUHStudent/IuhStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.IUHStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IuhStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
